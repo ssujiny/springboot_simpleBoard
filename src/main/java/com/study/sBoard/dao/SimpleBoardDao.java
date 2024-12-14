20ba502d864987e4a4fb81b7972043927a48dc18
ssujiny <aldebaran97@naver.com>
2024-12-14 23:13:06 +0900

    add comment after review

--- src/main/java/com/study/sBoard/dao/SimpleBoardDao.java
+++ src/main/java/com/study/sBoard/dao/SimpleBoardDao.java
@@ -9,7 +9,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.study.sBoard.dto.SimpleBoardDto;
-
+/*	@Repository 어노테이션
+ * 	스테레오 타입(stereo type) : 빈 등록하고 사용할때 개발자가 내부적으로 의미파악을 하기 쉽게 하려고 사용하는 별칭
+ * 	"이 클래스를 빈으로 등록하는데 DB관련 처리 용도로 사용하겠다."는 추가적인 의미 부여(스프링 내부에선 그냥 빈 등록)
+ */
 @Repository
 public class SimpleBoardDao implements ISimpleBoardDao{
 	
